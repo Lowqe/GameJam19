774405eb53d89f89bc69de8d643987cf5f02abe2
Simon <simon.sundstrom95@gmail.com>
2019-03-29 22:58:45 +0100

    kod

---
+++ GameJam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    float timer;
+
+    //Kanske ska vara gameobject istället för player
+    [SerializeField] private GameObject[] players;
+    [SerializeField] private GameObject[] spawnPoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+       for(int currentPlayer = 0; currentPlayer < players.Length; currentPlayer++)
+        {
+            Instantiate(players[currentPlayer], spawnPoints[currentPlayer].transform.position, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer = Time.deltaTime;
+        Debug.Log("Seconds: " + timer%60);
+        if(timer >= 3)
+        {
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        int mostCakes = -1;
+        int winner = -1;
+        for(int currentPlayer = 0; currentPlayer < players.Length; currentPlayer++)
+        {
+            if(players[currentPlayer].GetComponent<Player>().GetNumberOfCakes() > mostCakes)
+            {
+                mostCakes = players[currentPlayer].GetComponent<Player>().GetNumberOfCakes();
+                winner = currentPlayer;
+            }
+        }
+
+        Debug.Log("Player " + winner + " won!");
+    
+    }
+}
--- GameJam/Assets/Scripts/PowerUp.cs
+++ GameJam/Assets/Scripts/PowerUp.cs
@@ -6,14 +6,19 @@ public class PowerUp : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
+       
+        
         if (other.CompareTag("Player"))
         {
+            Player player = other.GetComponent<Player>();
+            player.AddCake();
             Pickup();
         }
     }
 
     void Pickup()
     {
+        Destroy(this.gameObject);
         Debug.Log("Power up picked up");
     }
 }

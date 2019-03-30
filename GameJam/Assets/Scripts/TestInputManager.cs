fe5454e694c7f86e014414dbf2f855ec9cf35261
Lowqe <33325524+Lowqe@users.noreply.github.com>
2019-03-30 15:15:16 +0100

    Revert "Revert "Added scripts""
    This reverts commit 4613bcc45dc83c05beb0906a5818ec2b169fbd23.

---
+++ GameJam/Assets/Scripts/TestInputManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestInputManager : MonoBehaviour
+{
+    private int cakeCount = 0;
+
+    [SerializeField] private float speed;
+
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
+    private Animator anim;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (InputManager.AButton())
+            Debug.Log("Success");
+
+        if (InputManager.AButtonTwo())
+            Debug.Log("MegaSuccess");
+
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("1J_MainHorizontal"), Input.GetAxisRaw("1J_MainVertical"));
+        moveVelocity = moveInput.normalized * speed;
+
+        if (moveInput.magnitude == 0)
+        {
+   //         anim.SetBool("isMoving", false);
+        }
+        else
+        {
+  //          anim.SetBool("isMoving", true);
+        }
+
+        if (cakeCount > 0 && Input.GetKeyDown(KeyCode.Space))
+        {
+            speedBoost();
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+
+    public void AddCake()
+    {
+        cakeCount++;
+    }
+    public int GetNumberOfCakes()
+    {
+        return cakeCount;
+    }
+
+    // if cakecount <0 and keydown x speed upp i x antal sekunder
+    private void speedBoost()
+    {
+        speed = 14f;
+    }
+}

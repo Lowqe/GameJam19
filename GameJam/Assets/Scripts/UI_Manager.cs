6336b370f30c35cb5d0eaa2f741422d53f3496d8
Hannovia <33325512+Hannovia@users.noreply.github.com>
2019-03-30 13:06:18 +0100

    PauseMenu

---
+++ GameJam/Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Manager : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+    public GameObject PauseMenuUI; 
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("StartButton"))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void MainMenu()
+    {
+
+    }
+
+    
+}
